e8fcbd2f00aeb0e46197b10d8998d4c3e93a1411
KaiwenHuu <kaiwenhu97@gmail.com>
2024-05-02 15:40:05 -0700

    fixed machine learning notes

--- src/pages/notes/machinelearning/index.js
+++ src/pages/notes/machinelearning/index.js
@@ -8,31 +8,21 @@ const MachineLearningPage = ( {data} ) => {
   return (
     <Layout pageTitle="Machine Learning">
       <p>
-      Machine Learning is a broad and dynamic field that spans from foundational statistical methods to advanced areas like Deep Learning and Generative AI. Given the expansive scope of this discipline, organizing thoughts and insights can be challenging. To bring clarity and structure to this complex subject, I have categorized my knowledge into distinct, somewhat orthogonal areas. If it gets overwhelming, I might split some of these into its own sections.</p>
+      Machine Learning is a broad and dynamic field that spans from foundational statistical methods to advanced areas like Deep Learning and Generative AI. Given the expansive scope of this discipline, organizing thoughts and insights can be challenging. To bring clarity and structure to this complex subject, I have categorized my knowledge into distinct, somewhat orthogonal areas. If it gets overwhelming, I might split some of these into its own sections.
+      </p>
       <ul>
-        <li>Linear Models</li>
-        <li>MLE and MAP</li>
-        <li>Optimization</li>
-        <li>Causal Models</li>
-        <li>Neural Network and Deep Learning</li>
-        <li>Convolutional Neural Network</li>
-        <li>Recurrent Neural Network</li>
-        <li>Reinforcement Learning</li>
-        <li>Transformers</li>
-        <li>Bayesian Regression and Gaussian Processes</li>
-        <li>VAE</li>
-      </ul>
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>
+            <li>
               <Link to={`/notes/${node.frontmatter.slug}`}>
                 {node.frontmatter.title}
               </Link>
-            </h2>
+            </li>
           </article>
         ))
       }
+      </ul>
     </Layout>
   )
 }
