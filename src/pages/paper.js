e480ce5bfabc0cf1817c445cac4a50edd50a9133
KaiwenHuu <kaiwenhu97@gmail.com>
2024-05-09 09:48:06 -0700

    added papers

--- src/pages/paper.js
+++ src/pages/paper.js
@@ -7,23 +7,32 @@ const PaperPage = () => {
   return (
     <Layout pageTitle="Papers">
       <p>
-        Here are some interesting papers I have read.
+        Here are some interesting papers I have read or plan to read.
       </p>
       <ul>
         <li>
-          <Link to="https://bpb-us-w2.wpmucdn.com/campuspress.yale.edu/dist/a/3019/files/2023/09/Master.pdf">Sequential Search</Link>
+          <Link to="https://bpb-us-w2.wpmucdn.com/campuspress.yale.edu/dist/a/3019/files/2023/09/Master.pdf">Spatial Learning and Path Dependence in Consumer Search</Link>
         </li>
         <li>
-          <Link to="https://arxiv.org/pdf/2202.05947">Auction</Link>
+          <Link to="https://arxiv.org/pdf/2202.05947">Artificial Intelligence and Auction Design</Link>
         </li>
         <li>
-          <Link to="https://arxiv.org/pdf/2202.00273">GAN</Link>
+          <Link to="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3682021">Algorithmic Pricing and Competition: Empirical Evidence from the German Retail Gasoline Market</Link>
         </li>
         <li>
-          <Link to="https://arxiv.org/pdf/1511.01844">Generative Models</Link>
+          <Link to="https://arxiv.org/abs/2205.04661">Pricing with Algorithms</Link>
         </li>
         <li>
-          <Link to="https://yang-song.net/blog/2021/score/">Generative Modeling</Link>
+          <Link to="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3195812">Autonomous Algorithmic Collusion: Q-Learning Under Sequential Pricing</Link>
+        </li>
+        <li>
+          <Link to="https://arxiv.org/pdf/2202.00273">StyleGAN-XL: Scaling StyleGAN to Large Diverse Datasets</Link>
+        </li>
+        <li>
+          <Link to="https://arxiv.org/pdf/1511.01844">A Note on the Evaluation of Generative Models</Link>
+        </li>
+        <li>
+          <Link to="https://yang-song.net/blog/2021/score/">Generative Modeling by Estimating Gradients of the Data Distribution</Link>
         </li>
         <li>
           <Link to="https://arxiv.org/pdf/2208.09392">Cold Diffusion</Link>
@@ -36,6 +45,9 @@ const PaperPage = () => {
         </li>
         <li>
           <Link to="https://arxiv.org/pdf/1512.03385">ResNet</Link>
+        </li>
+        <li>
+          <Link to="https://arxiv.org/pdf/2404.19756">Kolmogrov-Arnold Network</Link>
         </li>
       </ul>
     </Layout>
